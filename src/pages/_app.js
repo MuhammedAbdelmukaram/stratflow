904ef86f3baae17acef24cc2b9b4f0a0e523b848
Muhammed <muhammed.abdelmukaram@stu.ssst.edu.ba>
2023-11-02 07:04:24 +0100

    Fixed build issues

---
+++ src/pages/_app.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import "../assets/globals.css";
+
+
+const _app = ({ Component, pageProps }) => {
+
+    return <Component {...pageProps} />;
+};
+
+export default _app;
