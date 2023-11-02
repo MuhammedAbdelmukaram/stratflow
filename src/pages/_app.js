465194544ec3beef08de09e7c55c872b47b4919c
Muhammed <muhammed.abdelmukaram@stu.ssst.edu.ba>
2023-11-02 07:30:45 +0100

    Fixed build issues againn

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import "../assets/globals.css";
-
+import '../assets/CTA.css';      // Global CTA styles
+import '../assets/Signup.css';   // Global Signup styles
+import '../assets/analytics.css'; // Global Analytics styles
 
 
 const _app = ({ Component, pageProps }) => {
