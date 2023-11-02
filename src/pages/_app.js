0a5e40754adf311d3fe2046af22015e8815447a0
Muhammed <muhammed.abdelmukaram@stu.ssst.edu.ba>
2023-11-02 07:11:56 +0100

    Fixed build issues

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import "../assets/globals.css";
+import '../assets/CTA.css';
+import '../assets/Signup.css';
+import '../assets/analytics.css';
 
 
 const _app = ({ Component, pageProps }) => {
